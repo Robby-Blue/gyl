90c28708e4f94bfcabb1d758421f2fb79deaefa1
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2024-04-13 16:37:43 +0200

    add wildcard imports

---
+++ gyl/__init__.py
@@ -0,0 +1,13 @@
+from gyl.video import *
+from gyl.position import *
+from gyl.size import *
+from gyl.interpolation import *
+
+from gyl.elements.text import *
+from gyl.elements.latex import *
+from gyl.elements.striketrough import *
+
+from gyl.animations.move import *
+from gyl.animations.resize import *
+from gyl.animations.slidereveal import *
+from gyl.animations.edittext import *
