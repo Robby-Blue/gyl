e2a93be53423a5aa2ff86aaacd70225fe48a7804
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2024-04-14 01:52:45 +0200

    add right aligned position

--- gyl/position.py
+++ gyl/position.py
@@ -3,6 +3,11 @@ def center(position_val):
         return position_val-width_val/2
     return get_center
 
+def right(position_val):
+    def get_right(width_val):
+        return position_val-width_val
+    return get_right
+
 def x_of(element):
     def get_x_of(_):
         return element.normal_pos()[0]
